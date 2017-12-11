15bf700ba298cf90a9d45fba43cea37968f5abb7
Sahal Zain <zean00zone@gmail.com>
2017-12-11 16:00:59 +0700

    Update dockerfile

--- docker/cakeshop/cakeshop.sh
+++ docker/cakeshop/cakeshop.sh
@@ -1,9 +1,9 @@
 if [ ! -f data/local/application.properties ]; then
     if [ "$GETH_URL" = "" ]
     then
-        echo "geth.url=http\://localhost\:8545" >> application.properties
+        echo $'\n'geth.url=http\://localhost\:8545 >> application.properties
     else
-        echo "geth.url=" $GETH_URL >> application.properties
+        echo $'\n'geth.url= $GETH_URL >> application.properties
     fi
     mkdir -p data/local && cp application.properties data/local
 fi
